b252a4011e1510c0b937eb2bdd7100e0a1174e50
Administrator <jdchenok@126.com>
2019-09-07 15:19:21 +0800

    纪念第一个程序,中文版

---
+++ src/cn/jd/java/ChineseHelloWorld.java
@@ -0,0 +1,10 @@
+package cn.jd.java;
+
+/**
+ * 中文
+ */
+public class ChineseHelloWorld {
+    protected void test(){
+        System.out.println("你好世界!");
+    }
+}
--- src/cn/jd/java/HelloWorld.java
+++ src/cn/jd/java/HelloWorld.java
@@ -6,5 +6,6 @@ package cn.jd.java;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello,world!");
+        new ChineseHelloWorld().test();
     }
 }
